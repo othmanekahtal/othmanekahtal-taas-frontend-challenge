0f336d69d55f190245ad82553ec083e00212fea1
othmane kahtal <contact@othmanekahtal.me>
2022-03-21 00:16:24 +0100

    Add server folder🚀
    build backend for authorize to Github Account

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const superagent = require("superagent");
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const { CLIENT_ID: client_id, CLIENT_SECRET: client_secret } = process.env;
+app.get("/callback", async (req, res) => {
+  const { code } = req.query;
+  let { body } = await superagent.post(
+    "https://github.com/login/oauth/access_token",
+    {
+      client_id,
+      client_secret,
+      code,
+    }
+  );
+  res.redirect(`http://localhost:3000/auth/github?token=${body.access_token}`);
+});
+app.listen(8888, () => {
+  console.log(`The server is running at http://localhost:8888`);
+});
